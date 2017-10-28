7493c1a3ac3458aa8c5dae38506e7be84bdf2522
Tamim Shohid <tamimshohid@gmail.com>
2017-10-28 15:57:20 -0400

    still working to finish up

---
+++ Keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<HzBOrTFFd1RziJOpkQqnr15eG>',
+  consumer_secret: '<FO61CHFKd20nhl6y6SejD2QRuW5uIHbBRI1mZm8FIxUcK0IYX7>',
+  access_token_key: '<774617524721844224-sfFxmLRGKYd0qCbCX9vXsElEub94CRg>',
+  access_token_secret: '<	FysiR1dOjUsjPhicbA5VN5Vqtxm47ADRr60HgAm7Wos8u>',
+}
+
+module.exports = twitterKeys;
